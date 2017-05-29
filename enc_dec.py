9963a832bed7b293c33e9c7d12d89df9257f0278
nearstructure <tokoroten0401@gmail.com>
2017-05-29 13:18:20 +0900

    merge dictionaries

--- enc_dec.py
+++ enc_dec.py
@@ -1,36 +1,38 @@
 #  -*- coding: utf-8 -*-
 
 import pickle
+import unicodedata
 import numpy as np
 import chainer
 import chainer.functions as F
 import chainer.links as L
+from nltk import word_tokenize
 
 
 # definite encoder-decoder model
 class EncoderDecoder(chainer.Chain):
-    def __init__(self, post_size, cmnt_size, hidden):
+    def __init__(self, w_size, hidden):
         super(EncoderDecoder, self).__init__(
-            embedx=F.EmbedID(post_size, hidden),
-            embedy=F.EmbedID(cmnt_size, hidden),
+            embedx=F.EmbedID(w_size, hidden),
+            embedy=F.EmbedID(w_size, hidden),
             H=L.LSTM(hidden, hidden),
-            W=L.Linear(hidden, cmnt_size),
+            W=L.Linear(hidden, w_size),
         )
 
-    def __call__(self, post_line, cmnt_line, post_vocab, cmnt_vocab):
+    def __call__(self, post_line, cmnt_line, vocab):
         self.H.reset_state()
         for i in range(len(post_line)):
-            wid = post_vocab[post_line[i]]
+            wid = vocab[post_line[i]]
             x_k = self.embedx(chainer.Variable(np.array([wid], dtype=np.int32)))
             h = self.H(x_k)
-        x_k = self.embedx(chainer.Variable(np.array([post_vocab['<eos>']], dtype=np.int32)))
-        tx = chainer.Variable(np.array([cmnt_vocab[cmnt_line[0]]], dtype=np.int32))
+        x_k = self.embedx(chainer.Variable(np.array([vocab['<eos>']], dtype=np.int32)))
+        tx = chainer.Variable(np.array([vocab[cmnt_line[0]]], dtype=np.int32))
         h = self.H(x_k)
         accum_loss = F.softmax_cross_entropy(self.W(h), tx)
         for i in range(len(cmnt_line)):
-            wid = cmnt_vocab[cmnt_line[i]]
+            wid = vocab[cmnt_line[i]]
             x_k = self.embedy(chainer.Variable(np.array([wid], dtype=np.int32)))
-            next_wid = cmnt_vocab['<eos>'] if (i == len(cmnt_line) - 1) else cmnt_vocab[cmnt_line[i + 1]]
+            next_wid = vocab['<eos>'] if (i == len(cmnt_line) - 1) else vocab[cmnt_line[i + 1]]
             tx = chainer.Variable(np.array([next_wid], dtype=np.int32))
             h = self.H(x_k)
             loss = F.softmax_cross_entropy(self.W(h), tx)
@@ -41,58 +43,65 @@ class EncoderDecoder(chainer.Chain):
 def main():
 
     # load data
-    post_vocab = {}
+    vocab = {}
+    id2wd = {}
     post_lines = open('data/post.txt').read().split('\n')
     for i in range(len(post_lines)):
-        lt = post_lines[i].split()
+        lt = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(post_lines[i])]
         for w in lt:
-            if w not in post_vocab:
-                post_vocab[w] = len(post_vocab)
-    post_vocab['<eos>'] = len(post_vocab)
-    post_w_num = len(post_vocab)
+            if w not in vocab:
+                w_id = len(vocab)
+                vocab[w] = w_id
+                id2wd[w_id] = w
 
-    cmnt_vocab = {}
-    id2wd = {}
     cmnt_lines = open('data/cmnt.txt').read().split('\n')
     for i in range(len(cmnt_lines)):
-        lt = cmnt_lines[i].split()
+        lt = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(cmnt_lines[i])]
         for w in lt:
-            if w not in cmnt_vocab:
-                w_id = len(cmnt_vocab)
-                cmnt_vocab[w] = w_id
+            if w not in vocab:
+                w_id = len(vocab)
+                vocab[w] = w_id
                 id2wd[w_id] = w
-    w_id = len(cmnt_vocab)
-    cmnt_vocab['<eos>'] = w_id
+
+    post_test_lines = open('data/post-test.txt').read().split('\n')
+    for i in range(len(post_test_lines)):
+        lt = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(post_test_lines[i])]
+        for w in lt:
+            if w not in vocab:
+                w_id = len(vocab)
+                vocab[w] = w_id
+                id2wd[w_id] = w
+
+    w_id = len(vocab)
+    vocab['<eos>'] = w_id
     id2wd[w_id] = '<eos>'
-    cmnt_w_num = len(cmnt_vocab)
+    w_num = len(vocab)
 
     # save each dictionaries
-    with open('data/corpus/post-vocab.pkl', 'wb') as f:
-        pickle.dump(post_vocab, f)
-    with open('data/corpus/cmnt-vocab.pkl', 'wb') as f:
-        pickle.dump(cmnt_vocab, f)
+    with open('data/corpus/vocab.pkl', 'wb') as f:
+        pickle.dump(vocab, f)
     with open('data/corpus/id2wd.pkl', 'wb') as f:
         pickle.dump(id2wd, f)
 
     # create an instance of encoder-decoder model
     demb = 100
-    model = EncoderDecoder(post_w_num, cmnt_w_num, demb)
+    model = EncoderDecoder(w_num, demb)
     optimizer = chainer.optimizers.Adam()
     optimizer.setup(model)
 
     # start learning a model
     for epoch in range(100):
         for i in range(len(post_lines) - 1):
-            jln = post_lines[i].split()
+            jln = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(post_lines[i])]
             jlnr = jln[::-1]
-            eln = cmnt_lines[i].split()
+            eln = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(cmnt_lines[i])]
             model.H.reset_state()
             model.cleargrads()
-            loss = model(jlnr, eln, post_vocab, cmnt_vocab)
+            loss = model(jlnr, eln, vocab)
             loss.backward()
             loss.unchain_backward()     # truncate
             optimizer.update()
-        outfile = "data/mt-" + str(epoch) + ".model"
+        outfile = "data/conv-" + str(epoch) + ".model"
         chainer.serializers.save_npz(outfile, model)
         print(epoch, 'epoch')
 
--- test_model.py
+++ test_model.py
@@ -1,39 +1,42 @@
 #  -*- coding: utf-8 -*-
 
+import unicodedata
 import pickle
 import numpy as np
 import chainer
 import chainer.functions as F
+from nltk import word_tokenize
 from enc_dec import EncoderDecoder
 
 
 # load data
-with open('data/corpus/post-vocab.pkl', 'br') as f:
-    post_vocab = pickle.load(f)
-with open('data/corpus/cmnt-vocab.pkl', 'br') as f:
-    cmnt_vocab = pickle.load(f)
+with open('data/corpus/vocab.pkl', 'br') as f:
+    vocab = pickle.load(f)
 with open('data/corpus/id2wd.pkl', 'br') as f:
     id2wd = pickle.load(f)
-post_w_num = len(post_vocab)
-cmnt_w_num = len(cmnt_vocab)
+w_num = len(vocab)
 
 
 # output words
 def mt(model, post_line):
     model.H.reset_state()
     for i in range(len(post_line)):
-        # wid = post_vocab[post_line[i]]
-        wid = post_vocab.get(post_line[i], len(post_vocab)-1)
+        if vocab.get(post_line[i], None) is not None:
+            wid = vocab[post_line[i]]
+        else:
+            print(post_line[i])
+            print('this word is not in vocab.')
+            raise ValueError
         x_k = model.embedx(chainer.Variable(np.array([wid], dtype=np.int32), volatile='on'))
         h = model.H(x_k)
-    x_k = model.embedx(chainer.Variable(np.array([post_vocab['<eos>']], dtype=np.int32), volatile='on'))
+    x_k = model.embedx(chainer.Variable(np.array([vocab['<eos>']], dtype=np.int32), volatile='on'))
     h = model.H(x_k)
     wid = np.argmax(F.softmax(model.W(h)).data[0])
     sentence = ""
     sentence = sentence + id2wd[wid] + " "
     # print(id2wd[wid])
     loop = 0
-    while (wid != cmnt_vocab['<eos>']) and (loop <= 30):
+    while (wid != vocab['<eos>']) and (loop <= 30):
         x_k = model.embedy(chainer.Variable(np.array([wid], dtype=np.int32), volatile='on'))
         h = model.H(x_k)
         wid = np.argmax(F.softmax(model.W(h)).data[0])
@@ -48,11 +51,11 @@ post_test_lines = open('data/post-test.txt').read().split('\n')
 
 hidden = 100
 for epoch in range(100):
-    model = EncoderDecoder(post_size=post_w_num, cmnt_size=cmnt_w_num, hidden=hidden)
-    filename = "data/mt-" + str(epoch) + ".model"
+    model = EncoderDecoder(w_size=w_num, hidden=hidden)
+    filename = "data/conv-" + str(epoch) + ".model"
     chainer.serializers.load_npz(filename, model)
     for i in range(len(post_test_lines) - 1):
-        jln = post_test_lines[i].split()
+        jln = [unicodedata.normalize('NFKC', word.lower()) for word in word_tokenize(post_test_lines[i])]
         jnlr = jln[::-1]
         print(epoch, ":")
         mt(model, jnlr)
